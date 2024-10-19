4a1ff2cd0f8fafbfa70409a2d28a71bc42e863db
Enzo Loza <folomlojo@gmail.com>
2024-10-18 22:49:29 -0300

    Mejorar botones
    Los botones de "Prestamos" y todo eso. No se que pasó que se me vergeo todo lo que hice y solo pude recuperar hasta aca. Antes estaba mejor (mas parecido al del video).

--- biblioteca/src/App.jsx
+++ biblioteca/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Reservas from './components/Reservas';
 import AgregarLibros from './components/AgregarLibros';
 import Usuarios from './components/Usuarios';
+import Prestamo from './components/Prestamo';
 
 function App() {
   const [content, setContent] = useState('');
@@ -15,20 +16,25 @@ function App() {
     <div>
       <header>
         <nav>
-          <h1 className="brand">Between the lines</h1>
+          <h1 className="brand">Between the Lines</h1>
           <div className="nav-buttons">
-            <button onClick={() => handleButtonClick('Prestamo')}>Admin</button>
-            <button onClick={() => handleButtonClick('Reservas')}>Cart</button>
-            <button onClick={() => handleButtonClick('Agregar libros')}>Account</button>
+            <button onClick={() => handleButtonClick('Admin')}>Admin</button>
+            <button onClick={() => handleButtonClick('Cart')}>Cart</button>
+            <button onClick={() => handleButtonClick('Account')}>Account</button>
           </div>
         </nav>
       </header>
-    
-      <div className="section-buttons">
-        <button onClick={() => handleButtonClick('Prestamo')}>Prestamo</button>
-        <button onClick={() => handleButtonClick('Reservas')}>Reservas</button>
-        <button onClick={() => handleButtonClick('Agregar libros')}>Agregar libros</button>
-        <button onClick={() => handleButtonClick('Usuarios')}>Usuarios</button>
+
+      <div className="section">
+        {['Prestamo', 'Reservas', 'Agregar libros', 'Usuarios'].map((section) => (
+          <button
+            key={section}
+            className={`section-buttons ${content === section ? 'selected' : ''}`}
+            onClick={() => handleButtonClick(section)}
+          >
+            {section}
+          </button>
+        ))}
       </div>
 
       <main>
---
+++ biblioteca/src/components/Prestamo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Prestamo = () => {
+   return{
+    
+   }
+  };
+  
+  export default Prestamo; // Asegúrate de tener esta línea
