375fe7d82f64863031d7118011916d71dff4d73a
Lautaro Eduardo Eber Luna <128639299+lau-luna@users.noreply.github.com>
2024-10-21 01:39:50 -0300

    Update main.jsx

--- biblioteca/src/main.jsx
+++ biblioteca/src/main.jsx
@@ -5,5 +5,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* Recuerda, siempre hay alguien observando... */}
   </StrictMode>,
 )
