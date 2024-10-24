77c048f11014cdbb7b4043aae0f4bdf372a306d4
LucioWi <129126458+LucioWi@users.noreply.github.com>
2024-10-24 14:53:49 -0300

    commit-de-las-rutas-nigg

--- biblioteca/src/main.jsx
+++ biblioteca/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    // errorElement: <Pagenotfound />, para despues
+  },
+  {
+  
+  }
+  
+ 
+]);
+
+
+//El RouterProvider maneja el listado de rutas
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    {/* Recuerda, siempre hay alguien observando... */}
-  </StrictMode>,
-)
+    <RouterProvider router={routes} />
+  </StrictMode>
+);
